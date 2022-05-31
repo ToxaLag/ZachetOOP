f9492e1876b9771071589524c81a21c23e714738
ToxaLag <94950905+ToxaLag@users.noreply.github.com>
2022-05-31 20:20:52 +0800

    Add files via upload

--- Zachet/Zachet.cpp
+++ Zachet/Zachet.cpp
@@ -6,22 +6,81 @@ class Matrica
     int stroka, stolbez,rang;
     double deter;
     double **mas;
-    
 
+    Matrica &SwapStr(int stroka1, int stroka2)
+    {
+        double chislo;
+        for (int j = 0; j < stolbez; j++)
+        {
+            chislo = mas[stroka1][j];
+            mas[stroka1][j] = mas[stroka2][j];
+            mas[stroka2][j] = chislo;
+        }
+        return *this;
+    }
+
+    Matrica &SwapStl(int stolbez1, int stolbez2)
+
+    {
+        double chislo;
+        for (int i = 0; i < stroka; i++)
+        {
+            chislo = mas[stolbez1][i];
+            mas[stolbez1][i] = mas[stolbez2][i];
+            mas[stolbez2][i] = chislo;
+        }
+        return *this;
+    }
+
+    double Deter()
+    {
+        if (stroka != stolbez)
+        {
+            deter = 999333;
+            return deter;
+        }
+        deter = 1;
+        Matrica tmp(stroka, stolbez);
+        tmp = *this;
+        tmp = tmp.Diagonalize();
+        for (int i = 0; i < stroka; i++)
+        {
+            deter *= tmp.mas[i][i];
+        }
+        return deter;
+    }
+
+    bool ZeroCheck(int i_) //для строк
+    {
+        for (int j = 0; j < stroka; j++)
+            if (mas[i_][j] != 0)
+                return true;
+        return false;
+    }
+
+    int Rang()
+    {
+        rang = min(stroka, stolbez);
+        Matrica tmp(stroka, stolbez);
+        tmp = this->Diagonalize();
+        for (int i = 0; i < stroka; i++)
+        {
+            if (!tmp.ZeroCheck(i))
+                rang--;
+        }
+        return rang;
+    }
     
 public:
     Matrica(int stroka_, int stolbez_, int chislo = 0); //конструктор
     Matrica(const Matrica& ob); //конструктор коппий
-    ~Matrica()
-    {
-        for (int i = 0; i < stroka; ++i)
-            delete[] mas[i];
-        delete[] mas;
-        cout << "Деструктор;" <<endl;
-    } //деструктор*/
+    ~Matrica();   //объявление деструктора*/
     friend ostream& operator << (ostream& str, Matrica& ob);
     friend istream& operator >> (istream& str, Matrica& ob);
-    Matrica operator+(Matrica r_ob)
+    friend Matrica& operator*(double l_ob, Matrica r_ob);
+    double operator()(int stroka_, int stolbez_);
+
+    Matrica &operator+(Matrica &r_ob)
     {
         //сравнение размерности матриц //начало
         if ((this->stroka != r_ob.stroka) || (this->stolbez != r_ob.stolbez))
@@ -38,10 +97,12 @@ public:
                 tmp.mas[i][j] = this->mas[i][j] + r_ob.mas[i][j];
             }
         }
-        tmp.DeterGaus();
+        tmp.Deter();
+        tmp.Rang();
         return tmp;
     }
-    Matrica operator-(Matrica r_ob)
+
+    Matrica &operator-(Matrica &r_ob)
     {
         //сравнение размерности матриц //начало
         if ((this->stroka != r_ob.stroka) || (this->stolbez != r_ob.stolbez))
@@ -58,10 +119,12 @@ public:
                 tmp.mas[i][j] = this->mas[i][j] - r_ob.mas[i][j];
             }
         }
-        tmp.DeterGaus();
+        tmp.Deter();
+        tmp.Rang();
         return tmp;
     }
-    Matrica operator=(const Matrica& r_ob)
+
+    Matrica &operator=(const Matrica& r_ob)
     {
         for (int i = 0; i < stroka; ++i)
             delete[] mas[i];
@@ -77,7 +140,8 @@ public:
         }
         return *this;
     }
-    Matrica operator*(Matrica r_ob) // умножение матрицы на матрицу
+
+    Matrica &operator*(Matrica& r_ob) // умножение матрицы на матрицу
     {
         if (this->stroka != r_ob.stolbez)
         {
@@ -89,31 +153,34 @@ public:
             for (int j = 0; j < r_ob.stolbez; j++)
                 for (int k = 0; k < this->stolbez; k++)
                     tmp.mas[i][j] += this->mas[i][k] * r_ob.mas[k][j];
-        tmp.DeterGaus();
+        tmp.Deter();
+        tmp.Rang();
         return tmp;
     }  // умножение матрицы на матрицу конец;
-    Matrica operator*(double r_ob)
+
+    Matrica &operator*(double& r_ob)
     {
         Matrica tmp(this->stroka, this->stolbez, 0);
         for (int i = 0; i < tmp.stroka; i++)
             for (int j = 0; j < tmp.stolbez; j++)
                 tmp.mas[i][j] = this->mas[i][j] * r_ob;
-        tmp.DeterGaus();
+        tmp.Deter();
+        tmp.Rang();
         return tmp;
     }
-    double operator()(int stroka_ , int stolbez_);
-    friend Matrica operator*(double l_ob, Matrica r_ob);
+
     bool operator==(Matrica& r_ob) 
     {
         if ((this->stroka == r_ob.stroka) && (this->stolbez == r_ob.stolbez))
         {
             for (int i = 0; i < this->stroka; i++)
                 for(int j = 0; j < this->stolbez; j++)
-                    if (this->mas[i][j] == r_ob.mas[i][j])
+                    if (this->mas[i][j] != r_ob.mas[i][j])
                         return true;
         }
         return false;
     }
+
     double MaxOf()
     {
         double max;
@@ -129,6 +196,7 @@ public:
         cout << "Максимальный элемент [" << stroka_max + 1 << "][" << stolbez_max + 1 << "] = " << max << endl;
         return max;
     };
+
     double MinOf()
     {
         double min;
@@ -144,7 +212,8 @@ public:
         cout << "Максимальный элемент [" << stroka_max + 1 << "][" << stolbez_max + 1 << "] = " << min << endl;
         return min;
     };
-    Matrica Tranpon(bool p = false) //Метод транспонирования
+
+    Matrica &Tranpon(bool p = false) //Метод транспонирования
     {
         /*Если параметр false, то матрица транспонируется;
         Если true, то матрица передаётся */
@@ -166,99 +235,48 @@ public:
                 }
         return *this;
     }
+
     double GetDeter()
     {
-        DeterGaus();
+        cout << "Определитель матрицы = " << deter << endl;
         return deter;
     }
-    Matrica Revers()
+
+    bool GetRang()
     {
-        if ((stroka != stolbez) || (deter == 0))
-        {
-            cout << "Нет обратной матрицы";
-            return *this;
-        }
+        cout << "Ранг матрицы = " << rang << endl;
+        return rang;
     }
-    Matrica SwapStr(int stroka1, int stroka2)
+
+    Matrica Diagonalize()
     {
-        double chislo;
+        Matrica tmp(stroka,stolbez);
+        tmp = *this;
+        int stroka_max = 0;
         for (int j = 0; j < stolbez; j++)
         {
-            chislo = mas[stroka1-1][j];
-            mas[stroka1-1][j] = mas[stroka2-1][j];
-            mas[stroka2-1][j] = chislo;
-        }
-        return *this;
-    }
-    Matrica SwapStl(int stolbez1, int stolbez2)
-    {
-        double chislo;
-        for (int i = 0; i < stroka; i++)
-        {
-            chislo = mas[stolbez1-1][i];
-            mas[stolbez1-1][i] = mas[stolbez2-1][i];
-            mas[stolbez2-1][i] = chislo;
-        }
-        return *this;
-    }
-    double DeterGaus()
-    {
-        if (stroka != stolbez)
-        {
-            deter = 999333;
-            return deter;
-        }
-        deter = 1;
-        Matrica tmp(stroka, stroka);
-        tmp = *this;
-        double ches;
-        //проверка на нулевые стоки/столбцы
-        for (int i = 0; i < stroka; i++)
-        {
-            for (int j = 0; j < stroka; j++)
-            {
-                if (tmp.mas[i][j] != 0 || tmp.mas[j][i] != 0)
-                    break;
-                if ((j == stroka - 1) && (tmp.mas[i][j] == 0))
+            double max = tmp.mas[j][j];
+            double number = j;
+            for (int i = j; i < tmp.stroka; i++)
+                if (abs(tmp.mas[i][j]) >= max)
                 {
-                    deter = 0;
-                    return deter;
+                    max = tmp.mas[i][j];
+                    stroka_max = i;
+                }
+            tmp.SwapStr(j, stroka_max);
+            if (j != stroka_max)
+                tmp.deter *= -1;
+            for (int i = j+1; i < stroka; i++)
+            {
+                number = -tmp.mas[i][j] / tmp.mas[j][j];
+                for (int k = 0; k < stolbez; k++)
+                {
+                    tmp.mas[i][k] = tmp.mas[i][k] + tmp.mas[j][k] * number;
                 }
             }
         }
-        //конец проверки
-        //проверка на ненулевые элементы на глав диагонали + замена если возможно
-        int notNullMain = 1;
-        for (int i = 0; i < stroka; i++)
-        {
-            while (tmp.mas[i][i] == 0)
-            {
-                tmp.SwapStl(i, notNullMain);
-                notNullMain++;
-            }
-        }
-        //конец проверки
-        for (int k = 0; k < stroka; k++)
-            for (int i = k + 1; i < stroka; i++)
-            {
-                ches = -tmp.mas[i][k] / tmp.mas[k][k];
-                for (int j = 0; j < stroka; j++)
-                    tmp.mas[i][j] += tmp.mas[k][j] * ches;
-            }
-        for (int i = 0; i < stroka; i++)
-            deter *= tmp.mas[i][i];
-        return deter;
+        return tmp;
     }
-    //int Rang()
-    //{
-    //    rang = min(stroka, stolbez);
-    //    for (int i = 0; i < stroka; ++i)
-    //    {
-    //        for (int j = 0; j < stolbez; ++j)
-    //            return 0; //дописать
-    //    }
-    //}
-
 };
 
 int main()
@@ -267,7 +285,8 @@ int main()
     Matrica a(3, 3, 5);
     Matrica b(3, 3, 4);
     cin >> b;
-    cout << b.GetDeter();
+    b.GetDeter();
+    cout << b;
 
 }
 
@@ -293,8 +312,8 @@ Matrica::Matrica(int stroka_, int stolbez_, int chislo)  // конструкто
     for (int i = 0; i < stroka; i++)
         for (int j = 0; j < stolbez; j++)
             mas[i][j] = chislo;
-    deter = 0;
     rang = 1;
+    deter = 0;
 }                                                       // конструктор *конец
 
 Matrica::Matrica(const Matrica& ob)
@@ -308,10 +327,23 @@ Matrica::Matrica(const Matrica& ob)
             mas[i][j] = ob.mas[i][j];
 }
 
+Matrica::~Matrica()
+{
+    if (mas) // проверка памяти
+    {
+        for (int i = 0; i < stroka; ++i)
+            delete[] mas[i];
+        delete[] mas;
+    }
+} //деструктор*/
+
 double Matrica::operator()(int stroka_, int stolbez_)
 {
     if ((stroka_ < 1) || (stolbez_ < 1) || (stroka_ > stroka) || (stolbez_ > stolbez))
+    {
         cout << "Out of range" << endl;
+        return -999333;
+    }
     return mas[stroka_-1][stolbez_-1];
 }
 
@@ -337,11 +369,12 @@ istream& operator >> (istream& str, Matrica& ob)
             str >> ob.mas[i][j];
         }
     }
-    ob.DeterGaus();
+    ob.Deter();
+    ob.Rang();
     return str;
 }
 
-Matrica operator*(double l_ob, Matrica r_ob)
+Matrica &operator*(double l_ob, Matrica r_ob)
 {
     Matrica tmp(r_ob.stroka, r_ob.stolbez);
     for (int i = 0; i < tmp.stroka; i++)
